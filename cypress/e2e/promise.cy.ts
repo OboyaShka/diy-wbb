11dbf6f06a773012d79f3049328fe57df4a77c3d
Poznanskiy Denis <dpoznanskiy@yandex.ru>
2023-05-22 20:56:36 +0300

    custom promise

---
+++ cypress/e2e/promise.cy.ts
@@ -0,0 +1,41 @@
+import { Deferred } from "../../src/projects/promise/sources/core";
+
+describe(('test Deferred class'), () => {
+    let deferred: Deferred
+
+    beforeEach(() => {
+        deferred = new Deferred((resolve, reject) => {
+            setTimeout(() => {
+                resolve('test')
+            })
+        })
+    })
+
+    it('Deferred is created',() => {
+        expect(deferred instanceof Deferred).to.equal(true)
+    })
+
+    it('first then value',() => {
+        deferred.then(res => expect(res).to.equal('test'))
+        cy.wait(0)
+    })
+
+    it('chain of then', () => {
+        deferred.then(res => `${res} test`).then(res => expect(res).to.equal('test test'))
+        cy.wait(0)
+    })
+
+    it('return Deferred', () => {
+        deferred.then(res => new Deferred((resolve, reject) => {
+            setTimeout(() => {
+                resolve(`${res} test test`)
+            })
+        })).then(res => expect(res).to.equal('test test test'))
+        cy.wait(0)
+        cy.wait(0)
+    })
+})
+
+
+
+
---
+++ src/projects/promise/sources/core.ts
@@ -0,0 +1,28 @@
+
+export class Deferred<T = unknown> {
+    private callbacks: ((result: any) => void)[] = []
+
+    constructor(executor: (resolve: (value: T) => void, reject: (reason?: any) => void) => void) {
+        executor(this.resolve.bind(this), this.reject.bind(this))
+    }
+
+    public then<T = unknown>(cb: (result: T) => any): Deferred<T> {
+        return new Deferred((resolve, reject) => {
+            this.callbacks.push((prevValue) => {
+                const result = cb(prevValue)
+                if (result instanceof Deferred) {
+                    result.then(resolve)
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }
+
+    private resolve<T>(value: T): void {
+        this.callbacks.forEach(cb => cb(value))
+    }
+
+    private reject<T>(value: T): void {
+    }
+}
